d431a12bb5ac83515a307ca986f418dd68d926c3
LJ-Fryer <ljf_aus@hotmail.com>
2024-10-14 15:15:30 +1100

    Injected weather and time data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 function refreshWeather(response) {
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-  temperatureElement.innerHTML = Math.round(temperature);
-  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+
+  timeElement.innerHTML = formatDate(date);
   cityElement.innerHTML = response.data.city;
-  console.log(response.data);
+  temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
